07f2751d30401c7f8e00c32a3ea170a677472d81
sakamiyari <80993938+sakamiyari@users.noreply.github.com>
2021-12-18 23:27:51 +0900

    Add files via upload
    更新

--- count.js
+++ count.js
@@ -9,6 +9,7 @@ count=JSON.parse(count);
 count++;
 hyouji.innerText=count;
 localStorage.setItem("count",JSON.stringify(count));
+hendou();
 }
 function countdown(){
     count=JSON.parse(count);
@@ -18,6 +19,7 @@ function countdown(){
     }
     hyouji.innerText=count;
     localStorage.setItem("count",JSON.stringify(count));
+    hendou();
 }
 function rest(){
     //リセット
@@ -26,5 +28,44 @@ function rest(){
         count=0;
         hyouji.innerText=count;
         localStorage.setItem("count",JSON.stringify(count));
+        hendou();
+    }
+}
+//色違いの確率のプログラム
+let $page=document.getElementById("page");
+//DOMの操作は$という暗黙的なルールに基づく。
+//色違いの確率
+let kakuritu=1/4096
+let comb=0;//コンボレーションの値を保存させるやつ
+function kaku(){
+    let $namae=document.getElementById("namae");
+    if($namae.value=="色違いの出現する確率を非表示にする"){
+        $namae.value="色違いの出現する確率を表示する";
+    }else{
+        //表示するというテキストの場合
+        $namae.value="色違いの出現する確率を非表示にする";
+    }
+    hendou();
+}
+function hendou(){
+    //呼び出し用
+    let $namae=document.getElementById("namae");
+    if($namae.value=="色違いの出現する確率を非表示にする"){
+        if(count>0){
+            if(count==1){
+                let page=Math.floor(kakuritu*100*1000)/100;
+                $page.innerText="確率:"+page+"%";
+            }else{
+                //それ以外は計算する。
+                for(let i=1;i<count;i++){
+
+                }
+            }
+        }else{
+            //カウントが0だとエラー
+        $page.innerText="算出不可能です。"  
+        }
+    }else{
+        $page.innerText="";
     }
 }
